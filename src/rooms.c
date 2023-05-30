bd81f6f72c04928b3d45866f6c5395236c85c66b
kjh <wogudwkd1233@gmail.com>
2023-05-30 20:22:12 +0900

    타일 확인 용도로 임시방 생성

--- src/rooms.c
+++ src/rooms.c
@@ -16,18 +16,25 @@ bool is_door_ok(door d) {
 
 room * get_tmp_room() {
     room * rm;
-    int r, c;
+    int r, c, tv;
+    bool flg;
     rm = malloc(sizeof(room));
     memset(rm, 0, sizeof(rm));
 
-    rm->r = 7;
-    rm->c = 12;
+    rm->roff = 0;
+    rm->coff = 0;
     
+    rm->name = "Test Room";
+    rm->desc = "Room to see all present tiles";
+
+    rm->r = 1;
+    rm->c = TILE_NUM;
+
     rm->map = malloc(sizeof(tile *) * rm->r);
 
     for(r = 0; r < rm->r; ++r) {
         rm->map[r] = malloc(sizeof(tile) * rm->c);
-    }
+    } 
 
     rm->dirty = malloc(sizeof(bool *) * rm->r);
 
@@ -38,65 +45,17 @@ room * get_tmp_room() {
 
     for(r = 0; r < rm->r; ++r) {
         for(c = 0; c < rm->c; ++c) {
-            rm->map[r][c] = get_tile_template(TT_DARK);
-            rm->map[r][c].r = r;
-            rm->map[r][c].c = c;
-        }
-    }
-    
-    r = 1;
-    for(c = 1; c < 11; ++c) {
-        rm->map[r][c] = get_tile_template(TT_WOOD_WALL_HOR);
-        rm->map[r][c].r = r;
-            rm->map[r][c].c = c;
-    }
-    r = 5;
-    for(c = 1; c < 11; ++c) {
-        rm->map[r][c] = get_tile_template(TT_CAVE_WALL);
-        rm->map[r][c].r = r;
-        rm->map[r][c].c = c;
-    }
-
-    c = 1;
-    for(r = 2; r < 5; ++r) {
-        rm->map[r][c] = get_tile_template(TT_WOOD_WALL_VER);
-        rm->map[r][c].r = r;
-        rm->map[r][c].c = c;
-    }
-    c = 10;
-    for(r = 2; r < 5; ++r) {
-        rm->map[r][c] = get_tile_template(TT_CAVE_WALL);
-        rm->map[r][c].r = r;
-        rm->map[r][c].c = c;
-    }
-
-    for(r = 2; r < 4; ++r) {
-        for(c = 2; c < 10; ++c) {
-            rm->map[r][c] = get_tile_template(TT_CAVE_FLOOR);
-            rm->map[r][c].r = r;
-            rm->map[r][c].c = c;
+            ASSIGN_TILE_MACRO(TT_DARK, r, c);
         }
     }
 
-    for(r = 4; r < 5; ++r) {
-        for(c = 2; c < 10; ++c) {
-            rm->map[r][c] = get_tile_template(TT_GRASS_FLOOR);
-            rm->map[r][c].r = r;
-            rm->map[r][c].c = c;
-        }
+    r = 0;
+    for(c = 0; c < TILE_NUM; ++c) {
+        ASSIGN_TILE_MACRO(c, r, c);
     }
 
-    rm->map[1][5].status |= TS_FIRE;
-    rm->map[5][5].status |= TS_FIRE;
-    rm->map[3][1].status |= TS_FIRE;
-    rm->map[3][10].status |= TS_FIRE;
-
-    rm->map[2][5].status |= TS_FIRE;
-    rm->map[4][5].status |= TS_FIRE;
 
     rm->entities = NULL;
-
-    push_entity_into_room(rm, create_entity(ET_RABBIT), 4, 4, -1);
 
     return rm;
 }
